d12ef0d813b99393ceb22659852d7d158c016475
matthew <matthew@142.232.169.4>
2016-01-07 19:26:10 -0800

    Try #2

--- index.php
+++ index.php
@@ -5,76 +5,42 @@
         <title></title>
     </head>
     <body>
-   
+        <?php
+        $position = $_GET['board'];
+        $squares = str_split($position);
+        if (winner('x', $squares))
+            echo 'You win.';
+        else if (winner('o', $squares))
+            echo 'I win.';
+        else
+            echo 'No winner yet.';
+        ?>
     </body>
 
-</html>
-<?php
-$position = $_GET['board'];
-$squares = str_split($position);
+    <html>
 
-function winner($token, $position) {
-    $won = false;
-    //first row 
-    if (($position[0] == $token) &&
-        ($position[1] == $token) &&
-        ($position[2] == $token)){
-        $won = true;
-    }
-    //second row 
-    else if 
-        (($position[3] == $token) &&
-        ($position[4] == $token) &&
-        ($position[5] == $token)) {
-        $won = true;
-    }
-    //third row 
-       else if 
-        (($position[6] == $token) &&
-        ($position[7] == $token) &&
-        ($position[8] == $token)) {
-        $won = true;
-    }
-     //first column 
-       else if 
-        (($position[0] == $token) &&
-        ($position[3] == $token) &&
-        ($position[6] == $token)) {
-        $won = true;
-    }
-     //second column
-       else if 
-        (($position[1] == $token) &&
-        ($position[4] == $token) &&
-        ($position[7] == $token)) {
-        $won = true;
-    }
-     //third column
-       else if 
-        (($position[2] == $token) &&
-        ($position[5] == $token) &&
-        ($position[8] == $token)) {
-        $won = true;
-    }
-     //top left- bottom right diagonal
-       else if 
-        (($position[0] == $token) &&
-        ($position[4] == $token) &&
-        ($position[8] == $token)) {
-        $won = true;
-    }
-     //tbottom left - top right diagonal
-       else if 
-        (($position[6] == $token) &&
-        ($position[4] == $token) &&
-        ($position[2] == $token)) {
-        $won = true;
-    }
-    return $won;
-}
-?>
-  <?php   
-     if (winner('x',$squares)) echo 'You win.';
-     else if (winner('o',$squares)) echo 'I win.';
-     else echo 'No winner yet.';
-     ?>
+        <?php
+
+        function winner($token, $position) {
+            $won = false;
+            for ($row = 0; $row < 3; $row++) {
+                if (($position[3 * $row] == $token) && ($position[3 * $row + 1] == $token) && ($position[3 * row + 2] == $token))
+                    $won = true;
+            }
+            for ($col = 0; $col < 3; $col++) {
+                if (($position[$col] == $token) && ($position[$col + 3] == $token) && ($position[col + 6] == $token))
+                    $won = true;
+            }
+            if (($position[0] == $token) &&
+                    ($position[4] == $token) &&
+                    ($position[8] == $token)) {
+                $won = true;
+            } else if (($position[2] == $token) &&
+                    ($position[4] == $token) &&
+                    ($position[6] == $token)) {
+                $won = true;
+            }
+
+            return $won;
+        }
+        
